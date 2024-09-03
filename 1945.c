e62316a742d67605c61ffea8d01ce4cddddb8cd2
thomasjames433 <thomaspjames433@gmail.com>
2024-09-03 13:46:07 +0530

    1945

---
+++ 1945.c
@@ -0,0 +1,25 @@
+int getLucky(char* s, int k) {
+    int sum=0;
+    int i=0;
+    int x=0;
+    while(s[i]){
+        x=s[i]-'a'+1;
+        while(x!=0){
+            sum+=x%10;
+            x/=10;
+        }
+        i++;
+    }
+    x=0;
+    printf("%d",sum);
+    for(i=1;i<k;i++){
+        x=0;
+        while(sum!=0){
+            x+=sum%10;
+            sum/=10;
+        }
+        sum=x;
+        
+    }
+    return sum;
+}
