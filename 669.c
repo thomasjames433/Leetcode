501423de2704a54c18ff8f3fbf2fe83ffc61f52a
thomasjames433 <thomaspjames433@gmail.com>
2024-09-12 04:52:55 +0530

    .

---
+++ 669.c
@@ -0,0 +1,13 @@
+struct TreeNode* trimBST(struct TreeNode* root, int low, int high) {
+    if(!root)
+        return NULL;
+    if(root->val>high)
+        return trimBST(root->left,low,high);
+
+    if(root->val<low)
+        return trimBST(root->right,low,high);
+    
+    root->left=trimBST(root->left,low,high);
+    root->right=trimBST(root->right,low,high);
+    return root;
+}
