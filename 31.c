689d0bcb88b46513cb6a17642f0bdbdc5cad1754
thomasjames433 <thomaspjames433@gmail.com>
2025-01-31 05:27:53 +0530

    31

--- 31.c
+++ 31.c
@@ -1,39 +1,35 @@
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
 void nextPermutation(int* num, int numsize) {
-    int y=0;
-    int j;
+    int min=numsize-1;
     for(int i=numsize-1;i>0;i--){
         if(num[i]>num[i-1]){
-            for( j=numsize-1;j>=i;j--){
-                
-                if(num[j]>num[i-1]){
-                    int x=num[i-1];
-                    num[i-1]=num[j];
-                    num[j]=x;
-                    y=1;
-                }
-                if(y==1)
-                break;
+            while(num[i-1]>=num[min]){
+                min--;
             }
-            
-            
-        }
 
-        if(y==1){
-            while(i<numsize-1){
-                int x=num[i];
-                num[i]=num[numsize-1];
-                num[numsize-1]=x;
-                i++;numsize--;
-            };
-            break;
-        }
-        
+            swap(&num[i-1],&num[min]);
+            int x=i;
+            int y=numsize-1;
+            while(x<y){
+                swap(&num[x],&num[y]);
+                x++;y--;
+            }
+            return;
+        } 
+        // else{
+        //     min=i;
+        // }
     }
-    if(y==0){
-    for (int i=0;i<(numsize)/2;i++){
-        int x=num[i];
-        num[i]=num[numsize-1-i];
-        num[numsize-1-i]=x;
+    int x=0;
+    int y=numsize-1;
+    while(x<y){
+        swap(&num[x],&num[y]);
+        x++;y--;
     }
-    }
+    return;
 }
