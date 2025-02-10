683933e2b2574f716b0bdc6c77af0643e8fdd8ff
thomasjames433 <thomaspjames433@gmail.com>
2025-02-10 22:48:27 +0530

    1025

---
+++ 1025_doubt.c
@@ -0,0 +1,18 @@
+bool divisorGame(int n) {
+    if(n%2)
+        return 0;
+    return 1;
+}
+
+
+take the number 4
+
+alice choses 1 left with 3
+bob choses 1 left with 2
+alice choses 1 left with 1
+therefore alice wins 
+
+now 
+alice choses 2 left with 2
+bob choses 1 left with 1
+therefore bob wins
