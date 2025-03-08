f77b79f99db94a262745a93c37408a214e490fbe
thomasjames433 <thomaspjames433@gmail.com>
2025-03-08 12:40:07 +0530

    .

--- 141.c
+++ 141.c
@@ -6,43 +6,21 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    struct ListNode **list=(struct ListNode **)malloc(10000*sizeof(struct ListNode*));
-    struct ListNode *temp;
-    temp=head;
-    int x=0;
-    while(temp!=NULL){
-        int i=0;
-        while(i<x){
-            if(temp==list[i])
-                return true;
-            i++;
-        }
-        list[x]=temp;
-        x++;
-        temp=temp->next;
+    if(!head)
+        return 0;
+    struct ListNode*slow,*fast;
+    slow=head;
+    fast=head->next;
+    while(fast){
+        if(slow==fast)
+            return 1;
+        slow=slow->next;
+        fast=fast->next;
+        if(fast)
+            fast=fast->next;
+        else
+            return 0;
+        
     }
-    return false;
-}
-
-2nd method 
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-bool hasCycle(struct ListNode *head) {
-    if(head==NULL || !head->next)
-        return false;
-    struct ListNode *temp=head;
-    int i=0;
-    while(i<10002){
-        if(!temp)
-            return false;
-        temp=temp->next;
-        i++;
-    }
-    return true;
+    return 0;
 }
